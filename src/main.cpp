0ac0880a5eb05f3ccb4f3ed549dbbc7d6c994761
LumOs <leozhou2020@outlook.com>
2025-01-24 19:58:08 +0800

    test_servo

--- src/main.cpp
+++ src/main.cpp
@@ -27,16 +27,16 @@ int HTcount;
 int LDRLVal;
 int LDRMVal;
 int LDRRVal;
-
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 DHT dht(DHTPIN, DHTTYPE);
 ESP32PWM pwm;
+Servo servo;
+int pos = 0;
 
 void getHT() {
   ht.h = dht.readHumidity();
   ht.t = dht.readTemperature();
 }
-
 
 void displayHT() {
   tft.fillScreen(ST77XX_WHITE);
@@ -52,25 +52,51 @@ void displayHT() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  tft.initR(INITR_BLACKTAB);
-  Serial.begin(9600);
-  dht.begin();
-  tft.setRotation(4);
-  tft.fillScreen(ST77XX_WHITE);
-  tft.setTextColor(ST7735_BLACK);
-  tft.setTextSize(2);
-  pinMode(12, INPUT);
-  pwm.attachPin(ServoPIN, 500, 10);
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  servo.setPeriodHertz(50);
+  servo.attach(ServoPIN, 500, 2500);
+  // tft.initR(INITR_BLACKTAB);
+  // Serial.begin(9600);
+  // dht.begin();
+  // tft.setRotation(4);
+  // tft.fillScreen(ST77XX_WHITE);
+  // tft.setTextColor(ST7735_BLACK);
+  // tft.setTextSize(2);
+  pinMode(LDRLPIN, INPUT);  
+  pinMode(LDRMPIN, INPUT);
+  pinMode(LDRRPIN, INPUT);
+  // for(int pos = 0; pos <= 270; ++pos) {
+  //   servo.write(45);
+  //   delay(50);
+  //   if(analogRead(LDRMPIN) < LDRMVal) {
+  //     break;
+  //   }
+  //   else
+  //   {
+  //     LDRMVal = analogRead(LDRMPIN);
+  //   }
+    
+  // }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (HTcount >= 200000){
-    getHT();
-    displayHT();
-    HTcount = 0;
 
+void loop() {
+  // if (HTcount >= 200000){
+  //   getHT();
+  //   displayHT();
+  //   HTcount = 0;
+
+  // }
+  // ++HTcount;
+  for (pos = 0; pos <= 45; pos += 1) {
+    servo.write(90);
+    delay(15);
   }
-  ++HTcount;
+  for (pos = 45; pos >= 0; pos -= 1) {
+    servo.write(pos);
+    delay(15);
+  }
 }
